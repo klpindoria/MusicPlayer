34b895adfa7a505538c71566871bba9000952413
klpindoria <44475172+klpindoria@users.noreply.github.com>
2022-06-25 14:09:25 +0100

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Ketan Pindoria on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
